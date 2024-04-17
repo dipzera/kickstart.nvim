7f6907a12e9c4b00eafd3de263100b05063eecc6
Vlad Gorea <vladgorea@yahoo.com>
2024-04-17 11:33:18 +0300

    remove neogit

--- init.lua
+++ init.lua
@@ -870,17 +870,6 @@ require('lazy').setup {
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- { import = 'custom.plugins' },
-  {
-    'NeogitOrg/neogit',
-    dependencies = {
-      'nvim-lua/plenary.nvim', -- requried
-      'sindrets/diffview.nvim', -- optional
-
-      'nvim-telescope/telescope.nvim',
-    },
-    config = true,
-    opts = {},
-  },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
